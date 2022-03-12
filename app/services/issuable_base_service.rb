b814e52aa5bbd4ec45a79b439a5e2dfaf3676481
carlos <huaixian.huang+common@gmail.com>
2022-03-12 07:00:41 -0800

    Revert "Revert "Feat: only admin could assingee issues buy develper could assignee merge requests""
    This reverts commit 0ef7749588e41349c3a4c5a5cfcbd8ca3e2b3731.

--- app/services/issuable_base_service.rb
+++ app/services/issuable_base_service.rb
@@ -35,15 +35,6 @@ class IssuableBaseService < ::BaseProjectService
 
   def filter_params(issuable)
 
-    unless can?(current_user, :admin_project_member, issuable)
-      params.delete(:assignee_ids)
-      params.delete(:assignee_id)
-      params.delete(:due_date)
-      params.delete(:remove_assignee_ids)
-      params.delete(:milestone)
-      params.delete(:milestone_id)
-    end
-
     unless can_set_issuable_metadata?(issuable)
       params.delete(:milestone)
       params.delete(:milestone_id)
