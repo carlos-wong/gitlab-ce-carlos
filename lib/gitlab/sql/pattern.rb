a3a402f48d49154c9eb819b7361d137c2b528be4
carlos <huaixian.huang+common@gmail.com>
2022-12-17 08:31:53 -0800

    Fix: regex quoted word is changed to regex_quoted_term

--- lib/gitlab/sql/pattern.rb
+++ lib/gitlab/sql/pattern.rb
@@ -6,7 +6,7 @@ module Gitlab
       extend ActiveSupport::Concern
 
       MIN_CHARS_FOR_PARTIAL_MATCHING = 1
-      REGEX_QUOTED_WORD = /(?<=\A| )"[^"]+"(?= |\z)/.freeze
+      REGEX_QUOTED_TERM = /(?<=\A| )"[^"]+"(?= |\z)/.freeze
 
       class_methods do
         def fuzzy_search(query, columns, use_minimum_char_limit: true)
